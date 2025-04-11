652eb8b916516c0131de508e91c67c6063a2a547
Tún <ntuan.2502@gmail.com>
2025-04-11 09:36:49 +0700

    chore(nextjs): install @heroui/react and framer-motion

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+// tailwind.config.ts
+import { heroui } from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
+
+export default config;
